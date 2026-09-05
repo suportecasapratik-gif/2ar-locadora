97cfae16816fb16ca969c17a2d40321c62bcf084
suportecasapratik-gif <suportecasapratik@gmail.com>
2026-09-05 02:35:19 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,8 +1,7 @@
 // ==================================================================
 // Conexão com o Supabase.
 // ==================================================================
-const SUPABASE_URL = 'https://twnhwzuwankvzsrctvqh.supabase.co';
+const SUPABASE_URL = 'https://twnhwzuwankvzsrctvqh.supabase.co'; // <-- sua URL real
 const SUPABASE_ANON_KEY = 'sb_anonkey_ZWABTMRodGlCtgsF7KVKTw_6-Pi0EGK';
 
-// Cria a conexão com Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
