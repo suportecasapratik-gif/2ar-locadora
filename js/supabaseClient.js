077a8fbf73b81efb3129f067bc8595143a0495c4
suportecasapratik-gif <suportecasapratik@gmail.com>
2026-09-05 12:29:29 -0300

    Add files via upload

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,11 @@
 // ==================================================================
 // Conexão com o Supabase.
 // ==================================================================
-const SUPABASE_URL = 'https://twnhwzuwankvzsrctvqh.supabase.co'; // <-- sua URL real
+const SUPABASE_URL = 'https://twnhwzuwankvzsrctvqh.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_anonkey_ZWABTMRodGlCtgsF7KVKTw_6-Pi0EGK';
 
-const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Importa o Supabase
+const { createClient } = window.supabase;
+
+// Cria a conexão
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
